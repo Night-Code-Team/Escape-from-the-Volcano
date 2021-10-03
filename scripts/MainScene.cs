56572eddb91aaf21aabefa769c791890861f90b8
Maxim Bodrov <activer12@mail.ru>
2021-10-03 14:18:16 +0300

    0.0.004
    Переработаны тайлы, источники освещения

--- scripts/MainScene.cs
+++ scripts/MainScene.cs
@@ -3,18 +3,18 @@ using System.Collections.Generic;
 
 public class MainScene : Spatial
 {
-    private List<MeshInstance> roads = new List<MeshInstance>();
-    private int time = 0;
-    public int score { get; private set; }
-    public override void _Process(float delta)
-    {
-        time++;
-        score = time;
-    }
-    private MeshInstance AddNewTile(List<MeshInstance> roads)
-    {
-        // заготовка метода для генерации дороги
-        MeshInstance road = new MeshInstance();
-        return road;
-    }
+	private List<MeshInstance> roads = new List<MeshInstance>();
+	private int time = 0;
+	public int score { get; private set; }
+	public override void _Process(float delta)
+	{
+		time++;
+		score = time;
+	}
+	private MeshInstance AddNewTile(List<MeshInstance> roads)
+	{
+		// заготовка метода для генерации дороги
+		MeshInstance road = new MeshInstance();
+		return road;
+	}
 }
