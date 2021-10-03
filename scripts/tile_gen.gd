a1ecce47255032612debfb8dd52e02b58219a203
Maxim Bodrov <activer12@mail.ru>
2021-10-03 22:45:50 +0300

    0,0,6
    Запуск генератора тайлов

---
+++ scripts/tile_gen.gd
@@ -0,0 +1,47 @@
+extends Spatial
+
+var road_1 = load("res://scene/Tiles_road/road_1.tscn")
+var road_1_hole_1 = load("res://scene/Tiles_road/road_1_hole_1.tscn")
+var road_1_hole_2 = load("res://scene/Tiles_road/road_1_hole_2.tscn")
+var road_1_hole_3 = load("res://scene/Tiles_road/road_1_hole_3.tscn")
+var road_1_lava_1 = load("res://scene/Tiles_road/road_1_lava_1.tscn")
+var road_1_stone_1 = load("res://scene/Tiles_road/road_1_stone_1.tscn")
+var road_1_crack_1 = load("res://scene/Tiles_road/road_1_crack_1.tscn")
+var road_1_crack_2 = load("res://scene/Tiles_road/road_1_crack_2.tscn")
+var road_2 = load("res://scene/Tiles_road/road_2.tscn")
+var road_2_crack_1 = load("res://scene/Tiles_road/road_2_crack_1.tscn")
+var road_2_hole_1 = load("res://scene/Tiles_road/road_2_hole_1.tscn")
+var road_2_hole_2 = load("res://scene/Tiles_road/road_2_hole_2.tscn")
+var road_3 = load("res://scene/Tiles_road/road_3.tscn")
+var road_3_crack_1 = load("res://scene/Tiles_road/road_3_crack_1.tscn")
+
+var rng = RandomNumberGenerator.new()
+
+var array = [
+	road_1,
+	road_1_hole_1,
+	road_1_hole_2,
+	road_1_hole_3,
+	road_1_lava_1,
+	road_1_stone_1,
+	road_1_crack_1,
+	road_1_crack_2,
+	road_2,
+	road_2_crack_1,
+	road_2_hole_1,
+	road_2_hole_2,
+	road_3,
+	road_3_crack_1
+]
+func _ready():
+	rng.randomize()
+	_init_spawn()
+	pass
+
+func _init_spawn():
+	for i in range(100):
+		var my_random_number = floor(rng.randf_range(0, 6))
+		var new_road = array[my_random_number].instance()
+		new_road.translate(Vector3(0,-0.75,6.4*i))
+		add_child(new_road)
+		
