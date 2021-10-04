a6535befcf176867ae526a4935b2412ad2b95cd9
Alexander Kadykov <as.kadykov@mail.ru>
2021-10-04 23:25:54 +0300

    0.1.7
    рабочая версия

--- scripts/SlidingObjects.cs
+++ scripts/SlidingObjects.cs
@@ -2,10 +2,10 @@ using Godot;
 
 public class SlidingObjects : KinematicBody
 {
-	protected const int difficulty = 1;
+	protected const int difficulty = 0.2;
 	protected const int startvel = 5;
 	protected const int slide = 5;
 	protected int time = 0;
 	
-	protected float SetSpeed() => time / (100 * difficulty) + startvel;
+	protected float SetSpeed() => time * difficulty / 100 + startvel;
 }
